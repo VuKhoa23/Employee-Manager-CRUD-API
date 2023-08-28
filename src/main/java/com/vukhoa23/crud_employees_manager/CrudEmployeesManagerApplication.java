2d3d23951e18fb3bf2cbeb13a93473ab668db0c1
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-08-28 23:42:56 +0700

    first commit

---
+++ src/main/java/com/vukhoa23/crud_employees_manager/CrudEmployeesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vukhoa23.crud_employees_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vukhoa23/crud_employees_manager/CrudEmployeesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vukhoa23.crud_employees_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
